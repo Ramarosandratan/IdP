557770cf2a7b2638e127d30b3b44b035a815168a
Mampionona Rinasoa Ramarosandratana <ramarosandratana@hotmail.com>
2024-12-17 22:06:16 +0300

    init springboot
    Configuration du projet Spring Boot

---
+++ src/test/java/com/itu/idp/IdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itu.idp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
